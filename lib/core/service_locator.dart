f7c273e7a9079dc8864cb974ed5d68862f657b0b
Wairioko <charlesmungai5@gmail.com>
2024-02-16 16:13:47 +0300

    service locator.dart clean up

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -6,8 +6,7 @@ import '../data/repositories/data_sources/top_headlines_local_data_source.dart';
 import '../data/repositories/data_sources/top_headlines_remote_data_source.dart';
 import '../data/repositories/top_headlines_repository_implementation.dart';
 import '../domain/repositories/top_headlines_repository.dart';
-import 'package:daily_news/data/models/top_headlines_query_params.dart';
-import 'package:daily_news/ui/screens/home/widgets/query_widget.dart';
+
 
 GetIt sl = GetIt.instance;
 
