45679d0dc191566d17a7e4def5995923ba60f510
Wairioko <charlesmungai5@gmail.com>
2024-03-02 15:39:14 +0300

    improved login button and page look

--- lib/ui/screens/user_auth/login.dart
+++ lib/ui/screens/user_auth/login.dart
@@ -8,48 +8,56 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:safe_scan/ui/screens/user_auth/signup.dart';
 import 'package:safe_scan/provider/user_provider.dart';
 
-class LogInPage extends StatelessWidget {
+
+
+class LogInPage extends StatefulWidget {
   static const String path = '/login';
-  const LogInPage({Key? key});
+  const LogInPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final formkey = GlobalKey<FormState>();
-    TextEditingController email = TextEditingController();
-    TextEditingController password = TextEditingController();
+  _LogInPageState createState() => _LogInPageState();
+}
 
-    void moveToHome(BuildContext context) async {
-      if (formkey.currentState!.validate()) {
-        try {
-          await FirebaseAuth.instance.signInWithEmailAndPassword(
-              email: email.text, password: password.text);
-          context.read<UserProvider>().setUser(FirebaseAuth.instance.currentUser);
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const Home()));
-        } on FirebaseAuthException catch (e) {
-          if (e.code == "invalid-email") {
-            return showDialog(
-              context: context,
-              builder: (context) {
-                return const AlertDialog(
-                  content: Text("Please Enter A Valid Email"),
-                );
-              },
-            );
-          } else if (e.code == "wrong-password") {
-            return showDialog(
-              context: context,
-              builder: (context) {
-                return const AlertDialog(
-                  content: Text("Wrong Password"),
-                );
-              },
-            );
-          }
+class _LogInPageState extends State<LogInPage> {
+  final formkey = GlobalKey<FormState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  // Define a boolean variable to track whether the password is obscured or not
+  bool _obscurePassword = true;
+
+  void moveToHome(BuildContext context) async {
+    if (formkey.currentState!.validate()) {
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: email.text, password: password.text);
+        context.read<UserProvider>().setUser(FirebaseAuth.instance.currentUser);
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const Home()));
+      } on FirebaseAuthException catch (e) {
+        if (e.code == "invalid-email") {
+          return showDialog(
+            context: context,
+            builder: (context) {
+              return const AlertDialog(
+                content: Text("Please Enter A Valid Email"),
+              );
+            },
+          );
+        } else if (e.code == "wrong-password") {
+          return showDialog(
+            context: context,
+            builder: (context) {
+              return const AlertDialog(
+                content: Text("Wrong Password"),
+              );
+            },
+          );
         }
       }
     }
+  }
 
-
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
       body: SafeArea(
@@ -60,39 +68,40 @@ class LogInPage extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  //logo
                   Container(
-                      height: 250,
-                      child: Image.asset('assets/image/login.png')
+                    height: 250,
+                    child: Image.asset('assets/image/login.png'),
                   ),
-                  //Hello Again
                   Text(
                     'Hello Again! ',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 36,
+                      fontFamily: 'SanFrancisco'
+                    ),
                   ),
                   SizedBox(
                     height: 10,
                   ),
                   Text(
                     'Welcome back, you\'ve been missed! ',
-                    style: TextStyle(fontSize: 20),
+                    style: TextStyle(fontSize: 20, fontFamily: 'SanFrancisco'),
                   ),
                   SizedBox(
                     height: 50,
                   ),
-                  //email textfield
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 25.0),
                     child: Container(
                       decoration: BoxDecoration(
                         color: Colors.grey[200],
                         border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(12),
+                        borderRadius: BorderRadius.circular(30),
                       ),
                       child: Padding(
                         padding: const EdgeInsets.only(left: 5.0),
-                        child: TextFormField( // Use TextFormField instead of TextField
-                          controller: email, // Provide the TextEditingController
+                        child: TextFormField(
+                          controller: email,
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please enter your email';
@@ -110,30 +119,40 @@ class LogInPage extends StatelessWidget {
                   SizedBox(
                     height: 25,
                   ),
-                  //password
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 25.0),
                     child: Container(
                       decoration: BoxDecoration(
                         color: Colors.grey[200],
                         border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(12),
+                        borderRadius: BorderRadius.circular(30),
                       ),
                       child: Padding(
                         padding: const EdgeInsets.only(left: 5.0),
-                        child: TextFormField( // Use TextFormField instead of TextField
-                          controller: password, // Provide the TextEditingController
+                        child: TextFormField(
+                          controller: password,
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please enter your password';
                             }
                             return null;
                           },
-                          obscureText: true,
+                          obscureText: _obscurePassword,
                           decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.password),
-                              hintText: 'Password',
-                              border: InputBorder.none),
+                            prefixIcon: Icon(Icons.password),
+                            hintText: 'Password',
+                            border: InputBorder.none,
+                            suffixIcon: IconButton(
+                              icon: Icon(_obscurePassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                              onPressed: () {
+                                setState(() {
+                                  _obscurePassword = !_obscurePassword;
+                                });
+                              },
+                            ),
+                          ),
                         ),
                       ),
                     ),
@@ -141,46 +160,59 @@ class LogInPage extends StatelessWidget {
                   SizedBox(
                     height: 20,
                   ),
+                  CupertinoButton(
+                    onPressed: () {
+                      moveToHome(context);
+                    },
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.6, // Adjust the percentage as per your requirement
+                      height: MediaQuery.of(context).size.width * 0.1,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: CupertinoColors.activeBlue,
+                      ),
+                      padding: const EdgeInsets.all(4),
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: CupertinoColors.white,
+                          fontFamily: 'SanFrancisco',
+                        ),
+                      ),
+                    ),
+                  ),
 
-                  //signin button
-                  //TODO: Replace with MaterialButton
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: Size(300, 69),
-                        primary: Colors.lightBlueAccent,
-                        //padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                        textStyle: const TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.bold),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20))),
-                    onPressed: () {moveToHome(context);},
-                    child: Text('Sign In'),
-                  ),
-                  SizedBox(
-                    height: 25,
-                  ),
+
+                  SizedBox(height: 25),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
+                    children: [
                       Text(
                         'Not a member?',
                         style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Register Now',
-                        style: TextStyle(
                             fontSize: 15,
                             fontWeight: FontWeight.bold,
-                            color: Colors.blue),
-
-                      )
+                            color: CupertinoColors.black,
+                            fontFamily: 'SanFrancisco'),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/signup');
+                        },
+                        child: Text(
+                          ' Register Now',
+                          style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: CupertinoColors.activeBlue,
+                              fontFamily: 'SanFrancisco'),
+                        ),
+                      ),
                     ],
-                  )
-
-                  //not a member? register button
+                  ),
                 ],
               ),
             ),
