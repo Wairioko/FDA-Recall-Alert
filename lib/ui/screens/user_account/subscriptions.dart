8e45ef4a8971b907a0a51698732d3d0ad2fc4e48
Wairioko <charlesmungai5@gmail.com>
2024-03-25 20:39:45 +0300

    improved the ui loading time and added subscription logic

--- lib/ui/screens/user_account/subscriptions.dart
+++ lib/ui/screens/user_account/subscriptions.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:in_app_purchase/in_app_purchase.dart';
-import 'package:purchases_flutter/purchases_flutter.dart';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:purchases_flutter/purchases_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SubscriptionPackage {
   final String identifier; // Identifier for RevenueCat setup
@@ -50,7 +49,6 @@ Column buildPackageColumn(
   );
 }
 
-
 class SubscriptionPage extends StatefulWidget {
   static const String path = '/subscription_page';
 
@@ -59,92 +57,253 @@ class SubscriptionPage extends StatefulWidget {
 }
 
 class _SubscriptionPageState extends State<SubscriptionPage> {
-  int selectedIndex = 0; // Initially selecting the first package
-
-  late StreamSubscription<CustomerInfo> _purchaserInfoSubscription;
+  late Future<void> _initPlatformStateFuture;
   late List<Offering> _offerings;
   bool _isLoading = true;
+  int selectedIndex = 0;
   String? _error;
   final user = FirebaseAuth.instance.currentUser;
-
-
-  Future<void> _initPlatformState() async {
-    await Purchases.setDebugLogsEnabled(true);
-
-    PurchasesConfiguration configuration;
-    if (Platform.isAndroid) {
-      configuration = PurchasesConfiguration('goog_ZfrdtkQtiwLcpHvPjOUfvqxPqCq');
-    } else if (Platform.isIOS) {
-      configuration = PurchasesConfiguration('ios_app_user_id');
-    } else {
-      throw UnsupportedError('This platform is not supported.');
-    }
-    await Purchases.setup('goog_ZfrdtkQtiwLcpHvPjOUfvqxPqCq', appUserId: user?.uid);
-  }
 
   @override
   void initState() {
     super.initState();
-    _initPlatformState();
-    _offerings = []; // Initialize _offerings here
-    getOfferings();
-    Purchases.addCustomerInfoUpdateListener((customerInfo) {
-
-
-
-      // Handle successful transaction
-      // purchaserInfo contains information about the purchaser's subscription status
-      // You can update UI or navigate to another screen based on the subscription status
-
-      // Example: Update UI after successful transaction
-      setState(() {
-        // Update your UI here
-      });
-    });
-
+    _initPlatformStateFuture = _initPlatformState();
   }
 
-
-
-  Future<void> getOfferings() async {
-    print('Fetching offerings...');
+  Future<void> _initPlatformState() async {
     try {
-      Offerings offerings = await Purchases.getOfferings();
-      print('Offerings fetched successfully: $offerings');
-
-      if (offerings != null && offerings.all != null) {
-        setState(() {
-          _offerings = offerings.all.values.toList(); // Assign offerings directly
-          print('Available offerings: $_offerings');
-          print('Number of offerings: ${_offerings.length}');
-          print('keys keys keys ${offerings.all.keys}');
-
-          _isLoading = false;
-        });
+      await Purchases.setDebugLogsEnabled(true);
+      if (Platform.isAndroid) {
+        await Purchases.setup('goog_ZfrdtkQtiwLcpHvPjOUfvqxPqCq', appUserId: user?.uid);
+      } else if (Platform.isIOS) {
+        await Purchases.setup('ios_app_user_id', appUserId: user?.uid);
       } else {
-        // Handle case when there are no current offerings
-        setState(() {
-          _error = 'No offerings found.';
-          _isLoading = false;
-        });
+        throw UnsupportedError('This platform is not supported.');
       }
-    } on PlatformException catch (e) {
-      // ... existing error handling
+      await _fetchOfferings();
     } catch (e) {
-      // ... existing error handling
+      setState(() {
+        _isLoading = false;
+        _error = 'Error initializing: $e';
+      });
     }
   }
 
+  Future<void> _fetchOfferings() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      Offerings offerings = await Purchases.getOfferings();
+      setState(() {
+        _offerings = offerings.all.values.toList();
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _error = 'Error fetching offerings: $e';
+      });
+    }
+  }
+
+  Widget _buildLoading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _buildError() {
+    return Center(
+      child: Text(_error!),
+    );
+  }
+
+  Widget _buildOfferingTile(BuildContext context, Offering offering, bool isSelected, int index) {
+    final isSelected = index == selectedIndex;
+    final isYearlyPackage = offering.annual != null;
+    return GestureDetector(
+      onTap: () {
+        // Handle package selection
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 20),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.blueAccent : Colors.grey[200],
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: isSelected ? Colors.black26 : Colors.transparent,
+              blurRadius: 4,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            // Display monthly price
+            if (offering.monthly != null)
+              buildPackageColumn(
+                context,
+                'Monthly',
+                offering.monthly!.storeProduct.priceString,
+                isSelected,
+              ),
+            // Display 6 months price
+            if (offering.sixMonth != null)
+              buildPackageColumn(
+                context,
+                '6 Months',
+                offering.sixMonth!.storeProduct.priceString,
+                isSelected,
+              ),
+            // Display yearly price
+            if (offering.annual != null)
+              Expanded(
+                child: Stack(
+                  children: [
+                    buildPackageColumn(
+                      context,
+                      'Yearly',
+                      offering.annual!.storeProduct.priceString,
+                      isSelected,
+                    ),
+                      Positioned(
+                        right: 0,
+                        top: 0,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: const Text(
+                            'Best Value',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'SF Pro Text',
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOfferings() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Choose Your Plan',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SF Pro Text',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            itemCount: _offerings.length,
+            itemBuilder: (context, index) {
+              final offering = _offerings[index];
+              final isSelected = index == selectedIndex; // Adjust this based on your selection logic
+              return _buildOfferingTile(context, offering, isSelected, index);
+            },
+          ),
+        ),
+      ],
+    );
+  }
 
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Subscription Packages',
+          style: TextStyle(fontFamily: 'SF Pro Text'),
+        ),
+      ),
+      body: FutureBuilder<void>(
+        future: _initPlatformStateFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return _buildLoading();
+          } else if (snapshot.hasError) {
+            return _buildError();
+          } else {
+            return _isLoading
+                ? _buildLoading()
+                : _error != null
+                ? _buildError()
+                : _buildOfferings();
+          }
+        },
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.blue,
+            backgroundColor: Colors.blue,
+          ),
+          onPressed: () {
+            if (_offerings.isNotEmpty) {
+              // Select a default package if there are multiple available:
+              final selectedPackage = _offerings[0].availablePackages[0];
+              _purchasePackage(selectedPackage);
+            }
+          },
+          child: const Text(
+            'Subscribe',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'SF Pro Text',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   Future<void> _purchasePackage(Package package) async {
-      try {
+    try {
+      // Initiate the purchase process for the specified package
       CustomerInfo purchaserInfo = await Purchases.purchasePackage(package);
 
-      // Handle successful purchase. Here are a couple of things you'd likely do:
+      // Handle successful purchase
       if (purchaserInfo.entitlements.all["premium"]!.isActive) {
         // Unlock premium features based on the entitlement
         print('Purchase successful, premium features unlocked!');
@@ -174,175 +333,10 @@ class _SubscriptionPageState extends State<SubscriptionPage> {
 
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Subscription Packages',
-          style: TextStyle(fontFamily: 'SF Pro Text'),
-        ),
-      ),
-      body: _isLoading
-          ? const Center(
-        child: CircularProgressIndicator(),
-      )
-          : _error != null
-          ? Center(
-        child: Text(_error!),
-      )
-          : Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Choose Your Plan',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'SF Pro Text',
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.grey[300],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              itemCount: _offerings.length,
-              itemBuilder: (context, index) {
-                final offering = _offerings[index];
-
-
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      selectedIndex = index;
-                    });
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.only(bottom: 20),
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: selectedIndex == index
-                          ? Colors.blueAccent
-                          : Colors.grey[200],
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        selectedIndex == index
-                            ? const BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 4,
-                          offset: Offset(0, 2),
-                        )
-                            : const BoxShadow()
-                      ],
-                    ),
-                    child:Row(
-                      children: [
-                        // Display monthly price
-                        if (offering.monthly != null)
-                          buildPackageColumn(
-                            context,
-                            'Monthly',
-                            offering.monthly!.storeProduct.priceString,
-                            selectedIndex == index,
-                          ),
-                        // Display 6 months price
-                        if (offering.sixMonth != null)
-                          buildPackageColumn(
-                            context,
-                            '6 Months',
-                            offering.sixMonth!.storeProduct.priceString,
-                            selectedIndex == index,
-                          ),
-                        // Display yearly price
-                        if (offering.annual != null)
-                          Expanded(
-                            child: Stack(
-                              children: [
-                                buildPackageColumn(
-                                  context,
-                                  'Yearly',
-                                  offering.annual!.storeProduct.priceString,
-                                  selectedIndex == index,
-                                ),
-                                Positioned(
-                                  right: 0,
-                                  top: 0,
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                                    decoration: BoxDecoration(
-                                      color: Colors.green,
-                                      borderRadius: BorderRadius.circular(30),
-                                    ),
-                                    child: const Text(
-                                      'Best Value',
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: 'SF Pro Text',
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                      ],
-                    ),
-
-                  ));
-              },
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.blue,
-            backgroundColor: Colors.blue,
-          ),
-          onPressed: () {
-            if (_offerings.isNotEmpty) {
-              // Select a default package if there are multiple available:
-              final selectedPackage = _offerings[selectedIndex].availablePackages[0];
-              _purchasePackage(selectedPackage);
-            }
-          },
-          child: const Text(
-            'Subscribe',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'SF Pro Text',
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
   void dispose() {
     super.dispose();
   }
-
 }
+
 
 //sk_rUtnRFrwRXCagIcSPWhuHWWPfrXRh
