6972af0f8c5d0ab423be206b5fcd407cb2b3e7be
Wairioko <charlesmungai5@gmail.com>
2024-03-27 21:28:46 +0300

    put resukts for matches in tiles to improve the UI feel

--- lib/ui/screens/scan/scan_detail.dart
+++ lib/ui/screens/scan/scan_detail.dart
@@ -25,7 +25,6 @@ class SelectionScreen extends StatelessWidget {
     return inputText.replaceAll('\n', ' ').trim();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,38 +34,48 @@ class SelectionScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: matches.length,
         itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              // Navigate to detail screen for the selected match
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Detail(detailDataModel: matches[index]),
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: Material(
+              elevation: 4, // Add elevation for shadow
+              shadowColor: Colors.blueAccent,
+              borderRadius: BorderRadius.circular(12), // Add border radius
+              child: InkWell(
+                onTap: () {
+                  // Navigate to detail screen for the selected match
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Detail(detailDataModel: matches[index]),
+                    ),
+                  );
+                },
+                borderRadius: BorderRadius.circular(12),
+                splashColor: Colors.blueAccent,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        cleanText(matches[index].product_description),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      SizedBox(height: 4.0),
+                      Text(
+                        cleanText(matches[index].classification),
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 8.0), // Add some space between items
+                    ],
+                  ),
                 ),
-              );
-            },
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    cleanText(matches[index].product_description),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  SizedBox(height: 4.0),
-                  Text(
-                    cleanText(matches[index].classification),
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      color: Colors.black
-                    ),
-                  ),
-                  Divider(color: Colors.grey[400]), // Divider between items
-                ],
               ),
             ),
           );
