3017e59bcbfb07be436b9c7ee2e4d7e2aa288f78
Wairioko <charlesmungai5@gmail.com>
2024-04-29 21:44:53 +0300

    added recallEventProvider

--- lib/ui/screens/home/home.dart
+++ lib/ui/screens/home/home.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:provider/provider.dart';
+import '../../../core/news_application.dart';
 import '../../../core/news_provider.dart';
 import '../../shared/loading/loading_widget.dart';
 import '../../shared/theme/theme_cubit.dart';
 import 'widgets/menu_widget.dart';
 import '../../screens/home/widgets/dashboard_widget.dart';
+
 
 class Home extends StatefulWidget {
   static const String path = '/home';
@@ -18,40 +21,46 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late ZoomDrawerController drawerController;
+  late RecallEventProvider recallEventProvider;
 
   @override
   void initState() {
     super.initState();
     drawerController = ZoomDrawerController();
+    recallEventProvider = RecallEventProvider(RecallEventApplication(), const SizedBox());
   }
 
   @override
   Widget build(BuildContext context) {
-    return LoadingWidget(
-      loadingCubit: RecallEventProvider.loadingCubit,
-      child: Scaffold(
-        body: BlocBuilder<ThemeCubit, ThemeState>(
-          builder: (context, state) {
-            return ZoomDrawer(
-              controller: drawerController,
-              style: DrawerStyle.defaultStyle,
-              showShadow: true,
-              angle: 0.0,
-              openCurve: Curves.fastOutSlowIn,
-              closeCurve: Curves.bounceIn,
-              // slideWidth: 300,
-              moveMenuScreen: true,
-              menuScreen: const MenuWidget(),
-              mainScreen: DashBoardWidget(
-                zoomDrawerController: drawerController,
-              ),
-              borderRadius: 24.0,
-              mainScreenTapClose: true,
-              drawerShadowsBackgroundColor: Colors.greenAccent,
-              slideWidth: MediaQuery.of(context).size.width * .60,
-
-            );
-          },
+    return MultiProvider(  // Wrap Scaffold with MultiProvider
+      providers: [
+        Provider<ThemeCubit>(create: (_) => ThemeCubit()), // Create your ThemeCubit provider
+      ],
+      child: LoadingWidget(
+        loadingCubit: RecallEventProvider.loadingCubit,
+        child: Scaffold(
+          body: BlocBuilder<ThemeCubit, ThemeState>(
+            builder: (context, state) {
+              return ZoomDrawer(
+                controller: drawerController,
+                style: DrawerStyle.defaultStyle,
+                showShadow: true,
+                angle: 0.0,
+                openCurve: Curves.fastOutSlowIn,
+                closeCurve: Curves.bounceIn,
+                // slideWidth: 300,
+                moveMenuScreen: true,
+                menuScreen: const MenuWidget(),
+                mainScreen: DashBoardWidget(
+                  zoomDrawerController: drawerController,
+                ),
+                borderRadius: 24.0,
+                mainScreenTapClose: true,
+                drawerShadowsBackgroundColor: Colors.greenAccent,
+                slideWidth: MediaQuery.of(context).size.width * .60,
+              );
+            },
+          ),
         ),
       ),
     );
