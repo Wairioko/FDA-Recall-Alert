54742672dc90537989b673719470d4a91757bc88
Wairioko <charlesmungai5@gmail.com>
2024-04-29 21:41:03 +0300

    removed the double running of myapp, implemented logic to display the landing page once

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:safe_scan/ui/screens/detail/detail.dart';
+import 'package:safe_scan/ui/screens/home/home.dart';
 import 'package:safe_scan/ui/screens/home/widgets/landing_page.dart';
 import 'package:safe_scan/ui/screens/notifications/notifications_widget.dart';
+import 'package:safe_scan/ui/screens/receipts/view_receipts.dart';
+import 'package:safe_scan/ui/screens/scan/camera.dart';
+import 'package:safe_scan/ui/screens/user_account/account_security.dart';
+import 'package:safe_scan/ui/screens/user_account/feedback.dart';
+import 'package:safe_scan/ui/screens/user_account/subscriptions.dart';
+import 'package:safe_scan/ui/screens/user_account/user-account.dart';
 import 'package:safe_scan/ui/screens/user_auth/login.dart';
+import 'package:safe_scan/ui/screens/user_auth/signup.dart';
+import 'package:safe_scan/ui/screens/watchlist/watchlist_items.dart';
 import 'package:safe_scan/ui/shared/theme/theme_cubit.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -14,6 +24,8 @@ import 'core/service_locator.dart';
 import 'core/app.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'model/detail_data_model.dart';
 
 
 @pragma('vm:entry-point')
@@ -31,30 +43,18 @@ void main() async {
     statusBarIconBrightness: Brightness.dark,
   ));
 
-
-
   WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
-  runApp(MaterialApp(
-    home: isFirstTime ? LandingPage() : MyApp(),
-  ));
 
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-      apiKey:
-      "AIzaSyBpl9Bti-DNxFG4qbNf3n-YIlGpF7BPdUE", // paste your api key here
-      appId:
-      "1:529836025778:android:555c3ccf188bf01794a6ab", //paste your app id here
-      messagingSenderId: "529836025778", //paste your messagingSenderId here
-      projectId: "saferecall", //paste your project id here
+      apiKey: "AIzaSyBpl9Bti-DNxFG4qbNf3n-YIlGpF7BPdUE",
+      appId: "1:529836025778:android:555c3ccf188bf01794a6ab",
+      messagingSenderId: "529836025778",
+      projectId: "saferecall",
     ),
   );
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-
-
-  // Enable Crashlytics
   FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
 
   User? user = FirebaseAuth.instance.currentUser;
@@ -63,31 +63,77 @@ void main() async {
   application.onCreate();
   await setUpServiceLocators();
   await sl.allReady();
-  startAppComponent(application, user);
 
-
-
+  runApp(
+    MyApp(user: user),
+  );
 }
 
-void startAppComponent(var application, User? user) {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => UserProviderLogin()),
-        ChangeNotifierProvider<NotificationProvider>(
-          create: (_) => NotificationProvider(),
-        ),
+class MyApp extends StatelessWidget {
+  final User? user;
 
+  const MyApp({Key? key, this.user}) : super(key: key);
 
-
-
-      ],
-      child: BlocProvider(
-        create: (context) => ThemeCubit(),
-        child: RecallApp(application, user),
-
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FutureBuilder<bool>(
+        future: _checkFirstTime(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(); // or loading indicator
+          }
+          if (snapshot.hasError) {
+            return Container(); // handle error
+          }
+          final bool isFirstTime = snapshot.data ?? true;
+          return isFirstTime ? LandingPage() : Home();
+        },
       ),
+      routes: {
+        SignUpPage.path: (context) => const SignUpPage(),
+        '/login': (context) => const LogInPage(),
+        '/signup': (context) => const SignUpPage(),
+        '/home': (context) => const Home(),
+        '/user-account': (context) => const UserAccountPage(),
+        '/subscription_page': (context) => SubscriptionPage(),
+        '/feedback': (context) => FeedbackForm(),
+        '/account_security': (context) => AccountSettingsWidget(),
+        '/receipts': (context) => const ReceiptListScreen(),
+        '/notifications': (context) => NotificationsPage(),
+        '/camera': (context) => const MainScreen(),
+        NotificationDisplay.path: (context) {
+          final RemoteMessage message = ModalRoute.of(context)!.settings.arguments as RemoteMessage;
+          return NotificationDisplay(message: message);
+        },
+        WatchlistCategoryItemsScreen.path: (context) {
+          final String category = ModalRoute.of(context)?.settings.arguments as String;
+          return WatchlistCategoryItemsScreen(category: category);
+        },
+        Detail.path: (context) {
+          DetailDataModel detailDataModel = ModalRoute.of(context)?.settings.arguments as DetailDataModel;
+          return Detail(detailDataModel: detailDataModel);
+        }
+      },
+      builder: (context, child) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => UserProviderLogin()),
+            ChangeNotifierProvider<NotificationProvider>(
+              create: (_) => NotificationProvider(),
+            ),
+          ],
+          child: BlocProvider(
+            create: (context) => ThemeCubit(),
+            child: child,
+          ),
+        );
+      },
+    );
+  }
 
-    ),
-  );
+  Future<bool> _checkFirstTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('isFirstTime') ?? true;
+  }
 }
