64163052ef555551026382d3772c222a01d496e6
Wairioko <charlesmungai5@gmail.com>
2024-04-29 21:46:03 +0300

    fixed issue with loading cubit initialization

--- lib/core/news_provider.dart
+++ lib/core/news_provider.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
-
 import 'news_application.dart';
 import '../ui/shared/loading/loading_cubit.dart';
 
-class RecallEventProvider extends InheritedWidget {
 
+class RecallEventProvider extends InheritedWidget {
   static late RecallEventApplication appInstance;
-  static late LoadingCubit loadingCubit;
+  static late LoadingCubit _loadingCubit;
 
   final RecallEventApplication application;
 
   RecallEventProvider(this.application, Widget child, {super.key})
       : super(child: child) {
     appInstance = application;
+    _loadingCubit = LoadingCubit();
   }
 
+  static LoadingCubit get loadingCubit => _loadingCubit;
+
   @override
-  bool updateShouldNotify(_) => true;
+  bool updateShouldNotify(oldWidget) => true;
 
   static RecallEventProvider of(BuildContext context) {
-    return (context.dependOnInheritedWidgetOfExactType(aspect: RecallEventProvider) as RecallEventProvider);
+    return (context.dependOnInheritedWidgetOfExactType(
+        aspect: RecallEventProvider)
+    as RecallEventProvider);
   }
 }
